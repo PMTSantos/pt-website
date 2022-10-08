c99413db64fd5a59e316f9fad9f4ceb8c94f8e85
zDocks <46422686+zDocks@users.noreply.github.com>
2022-10-08 14:01:57 +0100

    Corrigido Mostrar inscrições pendentes como aluno
    Corrigido mostrar inscrições pendentes como aluno;
    Agora ao editar o aluno e escolher a turma aparece o nome da turma e não o ID

--- website/routes/admin.js
+++ website/routes/admin.js
@@ -23,7 +23,7 @@ router.post('/students', async (req, res) => {
         req.session.success = 'Utilizador eliminado com sucesso!'
     } else if (action == 'edit') {
         let { id, username, email, turmas, password, perms } = req.body
-        var sql = `UPDATE users SET username = ?, password = ?, email = ?, turmas = ?, perms = ? WHERE id = ?`
+        var sql = `UPDATE users SET username = ?, password = ?, email = ?, turma = ?, perms = ? WHERE id = ?`
         await global.db(sql, [username, password, email, JSON.stringify(turmas), perms, id])
 
         req.session.success = 'Utilizador editado com sucesso!'
